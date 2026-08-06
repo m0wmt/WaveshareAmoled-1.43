7027e6963c7544982e8962548bc927efe5662ff1
m0wmt <nobody@example.com>
2026-08-06 15:18:30 +0100

    Initial commit

---
+++ include/config_example.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD" 
+#define LOCAL_SSID "LOCAL SSID"
+#define LOCAL_WIFI_PASSWORD "LOCAL PASSWORD"
+#define SNTP_TIME_SERVER "pool.ntp.org"
